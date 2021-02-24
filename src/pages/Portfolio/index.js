4a24ba4e25e8cd700710e89e75bf3f3d005cd38b
Claire Cheek <claireccheek@gmail.com>
2021-02-23 21:51:17 -0800

    added portfolio projects

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -8,14 +8,21 @@ function Portfolio() {
             <div className="row">
                 <div className="col-md-4">
                     <div className="card">
-                        <img src="https://i.postimg.cc/ht1kFJ5Q/Screen-Shot-2021-01-04-at-4-59-38-PM.png" className="card-img-top"
+                        <img src="https://i.postimg.cc/NM5XBTXF/Screen-Shot-2021-02-23-at-9-34-52-PM.png" className="card-img-top"
                             alt="screenshot of MoodTunes landing page" />
                         <div className="card-body">
                             <h5 className="card-title">MoodTunes</h5>
                             <p className="card-text">A personalized playlist generator using YouTube and Unsplash APIs to help you lean into
             your feels.</p>
-                            <a href="https://tbone8098.github.io/uw-jagged-little-pill/" className="btn btn-dark">See it!</a><br /><br />
-                            <a href="https://github.com/Tbone8098/uw-jagged-little-pill" className="btn btn-dark">GitHub!</a>
+                            <div className="row">
+                                <div className="col">
+                                    <a href="https://tbone8098.github.io/uw-jagged-little-pill/" className="portfolio-btn btn btn-dark">See it!</a>
+                                </div>
+                                <div className="col">
+
+                                    <a href="https://github.com/Tbone8098/uw-jagged-little-pill" className="portfolio-btn btn btn-dark">GitHub!</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -23,14 +30,20 @@ function Portfolio() {
 
                 <div className="col-md-4">
                     <div className="card">
-                        <img src="https://i.postimg.cc/x1Rd7N58/Screen-Shot-2021-02-05-at-2-52-36-PM.png" className="card-img-top"
+                        <img src="https://i.postimg.cc/nLmTZyfN/Screen-Shot-2021-02-23-at-9-28-45-PM.png" className="card-img-top"
                             alt="screenshot of So Thirsty" />
                         <div className="card-body">
                             <h5 className="card-title">So Thirsty</h5>
                             <p className="card-text">An app thought up by yours truly and created by an awesome team to help you take care of
             your beloved house plants.</p>
-                            <a href="http://sothirstyproject.herokuapp.com/" className="btn btn-dark">See it!</a><br /><br />
-                            <a href="https://github.com/Calmanning/so_thirsty" className="btn btn-dark">GitHub!</a>
+                            <div className="row">
+                                <div className="col">
+                                    <a href="http://sothirstyproject.herokuapp.com/" className="btn btn-dark">See it!</a>
+                                </div>
+                                <div className="col">
+                                    <a href="https://github.com/Calmanning/so_thirsty" className="btn btn-dark">GitHub!</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,13 +51,80 @@ function Portfolio() {
 
                 <div className="col-md-4">
                     <div className="card">
-                        <img src="https://i.postimg.cc/Vs0TyMvs/Screen-Shot-2021-01-07-at-11-46-22-PM.png" className="card-img-top"
+                        <img src="https://i.postimg.cc/bvvD9b3g/Screen-Shot-2021-02-23-at-9-36-55-PM.png" className="card-img-top"
                             alt="screenshot of quiz page" />
                         <div className="card-body">
                             <h5 className="card-title">Schitt's Creek Quiz</h5>
                             <p className="card-text">Do you like Schitt's Creek? I do!</p>
-                            <a href="https://cccheek.github.io/hw_quiz/" className="btn btn-dark">See it!</a><br /><br />
-                            <a href="https://github.com/cccheek/hw_quiz" className="btn btn-dark">GitHub!</a>
+                            <div className="row">
+                                <div className="col">
+                                    <a href="https://cccheek.github.io/hw_quiz/" className="btn btn-dark">See it!</a>
+                                </div>
+                                <div className="col">
+                                    <a href="https://github.com/cccheek/hw_quiz" className="btn btn-dark">GitHub!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card">
+                        <img src="https://i.postimg.cc/BvxF6s79/Screen-Shot-2021-02-23-at-9-40-56-PM.png" className="card-img-top"
+                            alt="screenshot of MoodTunes landing page" />
+                        <div className="card-body">
+                            <h5 className="card-title">Note Taker</h5>
+                            <p className="card-text">Just a cute little note taker app to keep your thoughts and notes organized.</p>
+                            <div className="row">
+                                <div className="col">
+                                    <a href="https://ccc-note-taker.herokuapp.com/" className="btn btn-dark">See it!</a>
+                                </div>
+                                <div className="col">
+                                    <a href="https://github.com/cccheek/hw_note-taker" className="btn btn-dark">GitHub!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img src="https://i.postimg.cc/Vvrw4sCd/Screen-Shot-2021-02-23-at-9-46-45-PM.png" className="card-img-top"
+                            alt="screenshot of So Thirsty" />
+                        <div className="card-body">
+                            <h5 className="card-title">Day Planner</h5>
+                            <p className="card-text">A cute little day planner to help keep your schedule tight.</p>
+                            <div className="row">
+                                <div className="col">
+                                    <a href="https://cccheek.github.io/hw_day-planner/" className="btn btn-dark">See it!</a>
+                                </div>
+                                <div className="col">
+                                    <a href="https://github.com/cccheek/hw_day-planner" className="btn btn-dark">GitHub!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img src="https://i.postimg.cc/YSCs1pZg/Screen-Shot-2021-02-23-at-8-49-27-PM.png" className="card-img-top"
+                            alt="screenshot of quiz page" />
+                        <div className="card-body">
+                            <h5 className="card-title">Employee Directory</h5>
+                            <p className="card-text">Do you have employees? Would you like to be directed to them? Here you go!</p>
+                            <div className="row">
+                                <div className="col">
+                                    <a href="google" target="_blank" className="btn btn-dark">See it!</a>
+                                </div>
+                                <div className="col">
+                                    <a href="https://github.com/cccheek/react_employee_directory/" className="btn btn-dark">GitHub!</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
